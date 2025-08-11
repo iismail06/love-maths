2746d89f88fab7e791724e08b9eb0b6cdc8a3acb
idil <idili@hotmail.co.uk>
2025-08-11 12:45:57 +0100

    Display additions question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@
                 alert('You clicked the submit!');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked a ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame('addition'); // Start the game with addition by default
+
  })
  
  
@@ -23,10 +25,18 @@
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
     // Generate two random numbers between 1 and 25 
    let num1 = Math.floor(Math.random() * 25) + 1;
    let num2 = Math.floor(Math.random() * 25) + 1;
+
+   if(gameType === 'addition') {
+       displayAdditionQuestion(num1, num2);
+   } else {
+         alert(`Unknown game type: ${gameType}`);                   // Alert the user if the game type is unknown
+         throw `Unknown game type: ${gameType}. Aborting!`;         // Throw an error to stop the game
+   }
+  
 }
 
 // To check the user's answer
@@ -48,12 +58,19 @@ function incrementWrongAnswers() {
 }
 
 // To display an addition question
-function displayAdditionQuestion() {
-   
+function displayAdditionQuestion(operand1, operand2) {   // Display an addition question
+    // Get the element with the id of 'operation' and set its text content to the
+    document.getElementById('operand1').textContent = operand1;     // Set the first operand
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+
 }
 // To display a subtraction question
-function displaySubtractQuestion() {
-    
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '-';
+
 }
   // To display a multiplication question
 function displayMultiplyQuestion() {
