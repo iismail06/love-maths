3643fb0d0f711c94a42d763c99138d0224835e87
idil <idili@hotmail.co.uk>
2025-08-11 12:02:21 +0100

    Implement initial game logic functions in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+
+ // To start the game
+function runGame() {
+   
+}
+
+// To check the user's answer
+function checkAnswer() {
+    
+}
+
+// helper function  to calculate the number of correct answers
+function calculateCorrectAnswers() {
+    
+}
+// To increment the score
+function incrementScore() {
+    
+}
+// To increment the number of wrong answers
+function incrementWrongAnswers() {
+    
+}
+
+// To display an addition question
+function displayAdditionQuestion() {
+   
+}
+// To display a subtraction question
+function displaySubtractQuestion() {
+    
+}
+  // To display a multiplication question
+function displayMultiplyQuestion() {
+  
+} 
+
+// To display a division question
+function displayDivisionQuestion() {
+    
+}
